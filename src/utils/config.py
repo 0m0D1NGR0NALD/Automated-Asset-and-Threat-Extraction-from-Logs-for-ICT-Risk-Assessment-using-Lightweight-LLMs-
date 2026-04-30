92de9a161473e4a7306cea64869f8436f3ce50bc
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2026-04-30 22:05:17 +0200

    tool configuration

---
+++ src/utils/config.py
@@ -0,0 +1,23 @@
+import yaml
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    def __init__(self, config_path="config.yaml"):
+        with open(config_path, 'r') as f:
+            self.cfg = yaml.safe_load(f)
+    
+    def get(self, key, default=None):
+        keys = key.split('.')
+        value = self.cfg
+        for k in keys:
+            if isinstance(value, dict):
+                value = value.get(k, default)
+            else:
+                return default
+        return value
+    
+    def get_openai_key(self):
+        return os.getenv("OPENAI_API_KEY")
