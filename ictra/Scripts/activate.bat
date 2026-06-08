14509db058cecd7c740f90023430e6483a0807c6
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2026-06-08 15:16:17 +0200

    update venv files

--- ictra/Scripts/activate.bat
+++ ictra/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Pisa\ICTRA\Automated-Asset-Threat-Extraction-from-Logs-for-ICT-Risk-Assessment-using-Lightweight-LLMs-\ictra
+set "VIRTUAL_ENV=D:\Pisa\ICTRA\ictra-repo\ictra"
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -24,8 +24,8 @@ set PYTHONHOME=
 if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
-set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(ictra) 
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+set "VIRTUAL_ENV_PROMPT=(ictra) "
 
 :END
 if defined _OLD_CODEPAGE (
